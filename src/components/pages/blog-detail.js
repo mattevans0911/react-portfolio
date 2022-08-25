115c57fd80657fd729c2c967d078d6a408c7003b
mattevans0911 <102312529+mattevans0911@users.noreply.github.com>
2022-08-24 23:44:54 -0400

    added dropzone to blog form

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
-import ReactHtmlParser from "react-html-parser";
+import parse from "html-react-parser";
+import BlogFeaturedImage from "../blog/blog-featured-image";
 
 export default class BlogDetail extends Component {
   constructor(props) {
@@ -34,16 +35,15 @@ export default class BlogDetail extends Component {
   render() {
     const { title, content, featured_image_url, blog_status } =
       this.state.blogItem;
+
     return (
       <div className="blog-container">
         <div className="content-container">
           <h1>{title}</h1>
 
-          <div className="featured-image-wrapper">
-            <img src={featured_image_url} />
-          </div>
+          <BlogFeaturedImage img={featured_image_url} />
 
-          <div className="content">{ReactHtmlParser(content)}</div>
+          <div className="content">{parse(String(content))}</div>
         </div>
       </div>
     );
